48f5c2309c150f5c1a15d99ae9fe2c284ff1f085
disha-satija <dishasatija20@gmail.com>
2026-08-05 19:02:34 +0530

    Time: 19 ms (10.41%), Space: 46.6 MB (25.76%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length())
+        {
+            return false;
+        }
+        HashMap<Character, Integer> hm1 = new HashMap<>();
+        HashMap<Character, Integer> hm2 = new HashMap<>();
+        for(int i = 0; i < s.length(); i++)
+        {
+            char ch = s.charAt(i);
+            if(hm1.containsKey(ch))
+            {
+                hm1.put(ch, hm1.get(ch) + 1);
+            }
+            else
+            {
+                hm1.put(ch, 1);
+            }
+        }
+        for(int i = 0; i < t.length(); i++)
+        {
+            char ch = t.charAt(i);
+            if(hm2.containsKey(ch))
+            {
+                hm2.put(ch, hm2.get(ch) + 1);
+            }
+            else
+            {
+                hm2.put(ch, 1);
+            }
+        }
+       if(hm1.equals(hm2))
+       {
+        return true;
+       }
+       return false;
+    }
+}
