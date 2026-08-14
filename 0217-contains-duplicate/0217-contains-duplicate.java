06b70e518b8e9141f24fd7a6b7c944541f9ef983
disha-satija <dishasatija20@gmail.com>
2026-08-15 00:23:43 +0530

    Time: 20 ms (38.29%), Space: 108 MB (51.76%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,23 +1,20 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashMap<Integer, Integer> hm = new HashMap<>();
-        boolean found = false;
+        boolean ans = false;
         for(int num : nums)
         {
             if(hm.containsKey(num))
             {
                 hm.put(num, hm.get(num) + 1);
+                ans = true;
+                break;
             }
             else
             {
                 hm.put(num, 1);
             }
-            if(hm.get(num) > 1)
-            {
-                found = true;
-                break;
-            }
         }
-        return found;
+        return ans;
     }
 }
