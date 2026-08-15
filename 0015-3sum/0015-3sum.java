a5578639a92222a6d6e59d22e78867d1d75b6e39
disha-satija <dishasatija20@gmail.com>
2026-08-15 16:24:01 +0530

    Time: 33 ms (72.7%), Space: 59.2 MB (60.23%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -6,29 +6,36 @@ class Solution {
         for(int k = 0; k < n - 1; k++)
         {
             if(k > 0 && nums[k] == nums[k - 1])
-            continue;
+            {
+                continue;
+            }
             int i = k + 1;
             int j = n - 1;
             while(i < j)
             {
-                if(nums[i] + nums[j] + nums[k] == 0)
-                {
-                   ans.add(List.of(nums[i], nums[j], nums[k]));
-                   i++;
-                   j--;
-                   while(i < j && nums[i] == nums[i - 1])
-                    i++;
-                    while(i < j && nums[j] == nums[j + 1])
-                    j--;
-                }
-                else if(nums[i] + nums[j] + nums[k] < 0)
+            int sum = nums[i] + nums[j] + nums[k];
+            if(sum == 0)
+            {
+                ans.add(List.of(nums[i], nums[j], nums[k]));
+                i++;
+                j--;
+                while(i < j && nums[i] == nums[i - 1])
                 {
                     i++;
                 }
-                else
+                while(i < j && nums[j] == nums[j + 1])
                 {
                     j--;
                 }
+            }
+            else if(sum < 0)
+            {
+                i++;
+            }
+            else
+            {
+                j--;
+            }
             }
         }
         return ans;
